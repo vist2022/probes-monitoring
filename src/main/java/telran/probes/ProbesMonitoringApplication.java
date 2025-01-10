04f6b0dadf2d0fdd18d9b3af6b582db6cc43a80e
vist2022 <vitalistudy@yahoo.com>
2025-01-10 09:56:49 +0200

    created parent pom

---
+++ src/main/java/telran/probes/ProbesMonitoringApplication.java
@@ -0,0 +1,13 @@
+package telran.probes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbesMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbesMonitoringApplication.class, args);
+	}
+
+}
